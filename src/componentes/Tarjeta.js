1ca9b7b98421116156a266e38db89b8eb636abf2
ingsoutadet <aaron.soutadet@gmail.com>
2022-05-02 20:23:22 -0300

    listo

---
+++ src/componentes/Tarjeta.js
@@ -0,0 +1,19 @@
+import Item from "./Item";
+
+const listadoPlataformas = ["Twitter", "Facebook", "Youtube"];
+
+// CONSIGNAS EN TARJETA:
+// 1- importar Item
+// 2- utilizar el listadoPlataformas para renderizar la cantidad de Items acorde al array 🚩
+// 3- cada item debe tener como propiedades su key, pasar el metodo para elegir como favorita heradado por su padre y presentar el nombre de la plataforma
+
+export default function Tarjeta(props) {
+  return (
+   
+      <ul>
+        {listadoPlataformas.map((plataforma,index)=><li key={index+plataforma} onClick={props.elegirFavorita}>{plataforma}</li>)}
+      </ul>
+    
+    
+  );
+}
